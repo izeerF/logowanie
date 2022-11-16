c2f29b4f25d4c1cd254b86e06ba7954572b6b1b6
Kacper Hinz <1119374@teb-gdansk.ad>
2022-11-16 12:10:38 +0100

    reszta user.class

--- User.class.php
+++ User.class.php
@@ -1,17 +1,18 @@
 <?php
 class User {
+    private $db;
     private int $id;
     private string $login;
     private string $email;
-    private string $passwordHash;
-    private string $firstName;
-    private string $lastName;
+    private string $haslo;
+    private string $imie;
+    private string $nazwisko;
 
-    public function __construct(string $email, string $password) {
-        $this->login = $email;
-        $this->password = $password;
+    public function __construct(string $email, string $haslo) {
+        $this->email = $email;
+        $this->haslo = $haslo;
         global $db;
-        $this->$db = $db;
+        $this->db = &$db;
     }
     public function isAuth() : bool {
         if(isset($this->id) && $this->id != null) {
@@ -25,12 +26,34 @@ class User {
         $preperedQ->bind_param('s', $this->email);
         $preperedQ->execute();
         $result = $preperedQ->get_result();
+        $row = $result->fetch_assoc();
+        if(password_verify($this->haslo, $row['haslo'])) {
+            $this->id = $row['id'];
+            $this->imie = $row['imie'];
+            $this->nazwisko = $row['nazwisko'];
+        }
     }
     public function logout() {
-        
+        $this->email = null;
+        $this->login = null;
+        $this->haslo = null;
+        $this->id = null;
+        $this->imie = null;
+        $this->nazwisko = null;
     }
     public function register() {
-        
+        $q = 
+            "INSERT INTO uzytkownicy 
+            (id, email, haslo, login, imie, nazwisko)
+            VALUES (NULL, ?, ?, ?, ?, ?)";
+        $pQ = $this->db->prepare($q);
+        $passwordHash = password_hash($this->haslo, PASSWORD_ARGON2I);
+        if(!isset($this->imie))
+            $this->imie = "";
+        if(!isset($this->nazwisko))
+            $this->nazwisko = "";
+        $pQ -> bind_param("sssss", $email, $haslo, $login, $imie, $nazwisko);
+        $pQ->execute();
     }
 }
 
--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 require('User.class.php');
-$db = new mysqli("localhost", "root", "", "stronalogowanie");
-$user - new User("123@gmail.com","12341234");
+$db = new mysqli('localhost', 'root', '', 'stronalogowanie');
+$user = new User("123@gmail.com", "12341234");
 $user->login();
+var_dump($user);
 ?>
