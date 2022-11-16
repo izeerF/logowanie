6b21bdd7af48ff94a3ba874ae909315b02a0234c
Kacper Hinz <1119374@teb-gdansk.ad>
2022-11-16 11:07:02 +0100

    klasy

--- User.class.php
+++ User.class.php
@@ -6,6 +6,24 @@ class User {
     private string $passwordHash;
     private string $firstName;
     private string $lastName;
+
+    public function __construct(string $email, string $password) {
+        $this->login = $email;
+        $this->password_hash = password_hash($password, PASSWORD_ARGON2I);
+        $this->$db = $db;
+    }
+    public function isAuth() {
+
+    }
+    public function login() {
+        
+    }
+    public function logout() {
+        
+    }
+    public function register() {
+        
+    }
 }
 
 
