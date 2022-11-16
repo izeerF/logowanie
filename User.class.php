78f3d18097f0972b82caf2bc45710496c8256dfd
Kacper Hinz <1119374@teb-gdansk.ad>
2022-11-16 10:58:16 +0100

    register

---
+++ User.class.php
@@ -0,0 +1,12 @@
+<?php
+class User {
+    private int $id;
+    private string $login;
+    private string $email;
+    private string $passwordHash;
+    private string $firstName;
+    private string $lastName;
+}
+
+
+?>
